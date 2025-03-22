452d01172b46e71e9de97f9c7f319b47aa257eeb
pcnslt <info@pcnslt.com>
2025-03-22 19:33:11 -0400

    Add TERMINATE command to assistant-proxy agents. Max_turns=4

--- assistant_proxy_agent.py
+++ assistant_proxy_agent.py
@@ -16,22 +16,31 @@ llm_config = {
 assistant = AssistantAgent(
     name="assistant",
     llm_config=llm_config,  # <-- crucial keyword argument
-    max_consecutive_auto_reply=None
+    max_consecutive_auto_reply=None,
+    system_message="""
+    You are the best assitant in the world, and extremely intelligent.
+    You think in numbers and facts, are extremely precise.
+    When you see the message TERMINATE, then end the conversation
+    """
 )
 
 user_proxy = UserProxyAgent(
     name="user_proxy",
     llm_config=llm_config,  # <-- this gives it LLM access
-    code_execution_config=False,
+    code_execution_config={
+        "word_dir": "code_execution",
+        "use_docker":False
+        },
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=None
+    max_consecutive_auto_reply=None,
+    system_message="When you get your answer, end the conversation by saying TERMINATE"
 )
 
 # Start the chat
 fullConversation =  user_proxy.initiate_chat(
     assistant,
     message="What's the capital of France?",  # Corrected question
-    max_turns=2
+    max_turns=4
 )
 
 st.text(fullConversation)
